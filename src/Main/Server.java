4a2873fe403faff62b001e2374d316f4663b5b6a
rnarcin <marcin_o@ymail.com>
2017-12-06 02:45:05 +0100

    Server evolves.

--- src/Main/Server.java
+++ src/Main/Server.java
@@ -6,30 +6,16 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * A server program which accepts requests from clients to
- * capitalize strings.  When clients connect, a new thread is
- * started to handle an interactive dialog in which the client
- * sends in a string and the server thread sends back the
- * capitalized version of the string.
- *
- * The program is runs in an infinite loop, so shutdown in platform
- * dependent.  If you ran it from a console window with the "java"
- * interpreter, Ctrl+C generally will shut it down.
- */
 public class Server {
 
-    /**
-     * Application method to run the server runs in an infinite loop
-     * listening on port 9898.  When a connection is requested, it
-     * spawns a new thread to do the servicing and immediately returns
-     * to listening.  The server keeps a unique client number for each
-     * client that connects just to show interesting logging
-     * messages.  It is certainly not necessary to do this.
-     */
+    private static ArrayList<Socket> socketList = new ArrayList<Socket>();
+
     public static void main(String[] args) throws Exception
     {
+
         System.out.println("The server is running.");
         int clientNumber = 0;
         ServerSocket listener = new ServerSocket(6969);
@@ -45,11 +31,6 @@ public class Server {
         }
     }
 
-    /**
-     * A private thread to handle capitalization requests on a particular
-     * socket.  The client terminates the dialogue by sending a single line
-     * containing only a period.
-     */
     private static class Player extends Thread
     {
         private Socket socket;
@@ -58,38 +39,23 @@ public class Server {
         public Player(Socket socket, int clientNumber)
         {
             this.socket = socket;
+            socketList.add(socket);
             this.clientNumber = clientNumber;
             log("New connection with player# " + clientNumber + " at " + socket);
         }
 
-        public Socket getSocket()
-        {
-            return socket;
-        }
-
-        /**
-         * Services this thread's client by first sending the
-         * client a welcome message then repeatedly reading strings
-         * and sending back the capitalized version of the string.
-         */
         public void run()
         {
             try
             {
-
-                // Decorate the streams so we can send characters
-                // and not just bytes.  Ensure output is flushed
-                // after every newline.
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(socket.getInputStream()));
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
                 // Send a welcome message to the client.
-                out.println("Hello, you are player #" + clientNumber + ".");
-                out.println("Enter exit to quit\n");
+                out.println("Hello, you are player#" + clientNumber + ".");
+                out.println("Enter exit to quit.\n");
 
-                // Get messages from the client, line by line; return them
-                // capitalized
                 while (true)
                 {
                     String input = in.readLine();
@@ -97,7 +63,12 @@ public class Server {
                     {
                         break;
                     }
-                    out.println(input);
+                    for(int i=0;i<socketList.size();i++)
+                    {
+                        //System.out.println(input);
+                        PrintWriter out1 = new PrintWriter(socketList.get(i).getOutputStream(), true);
+                        out1.println("player#"+clientNumber+": "+input);
+                    }
                 }
             }
             catch (IOException e)
@@ -118,10 +89,6 @@ public class Server {
             }
         }
 
-        /**
-         * Logs a simple message.  In this case we just write the
-         * message to the server applications standard output.
-         */
         private void log(String message)
         {
             System.out.println(message);
