272070631ad907a5028de3e413229e6348082f00
AgnieszkaMarta <32687852+AgnieszkaMarta@users.noreply.github.com>
2017-12-06 10:24:09 +0100

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args) throws Exception {
         Client client = new Client();
-        //client.connectToServer();
+        client.connectToServer();
         client.start();
 
     }
