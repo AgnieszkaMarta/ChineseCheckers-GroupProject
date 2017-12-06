358c82dd6e930264d3850027f4c391f6389d8469
AgnieszkaMarta <agnieszkamartaguzik@gmail.com>
2017-12-06 09:57:38 +0100

    123

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,7 @@ public class Main
 {
     public static void main(String[] args) throws Exception {
         Client client = new Client();
+        //client.connectToServer();
         client.start();
 
     }
