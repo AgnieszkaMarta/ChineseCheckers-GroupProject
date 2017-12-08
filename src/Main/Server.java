d8498c7482e6c5bfbee5513f65be01d180cd018e
rnarcin <marcin_o@ymail.com>
2017-12-08 15:01:43 +0100

    -Created GUI class.

--- src/Main/Server.java
+++ src/Main/Server.java
@@ -7,22 +7,26 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.List;
 
-public class Server {
+public class Server
+{
 
-    private static ArrayList<Socket> socketList = new ArrayList<Socket>();
+    private static ArrayList<Player> players = new ArrayList<>();
 
     public static void main(String[] args) throws Exception
     {
-
+        Player player;
         System.out.println("The server is running.");
-        int clientNumber = 0;
         ServerSocket listener = new ServerSocket(6969);
+        Socket client;
         try {
             while (true)
             {
-                new Player(listener.accept(), clientNumber++).start();
+                client = listener.accept();
+                player = new Player(client, players.size());
+                player.start();
+                players.add(player);
+
             }
         }
         finally
@@ -31,15 +35,19 @@ public class Server {
         }
     }
 
-    private static class Player extends Thread
+    public static class Player extends Thread
     {
         private Socket socket;
         private int clientNumber;
+        private String nickname;
+        private BufferedReader in;
+        private PrintWriter out;
+        private String input;
+        private String nick;
 
         public Player(Socket socket, int clientNumber)
         {
             this.socket = socket;
-            socketList.add(socket);
             this.clientNumber = clientNumber;
             log("New connection with player# " + clientNumber + " at " + socket);
         }
@@ -48,26 +56,29 @@ public class Server {
         {
             try
             {
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(socket.getInputStream()));
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+
+                nick=(in.readLine());
+
 
                 // Send a welcome message to the client.
-                out.println("Hello, you are player#" + clientNumber + ".");
-                out.println("Enter exit to quit.\n");
+                out.println("Hello, " + nick + "!");
+                out.println("Enter exit to quit.\n---------------------------");
 
                 while (true)
                 {
-                    String input = in.readLine();
+                    input = in.readLine();
+                    log(input);
+
                     if (input.equals("exit"))
                     {
+                        players.remove(this);
                         break;
                     }
-                    for(int i=0;i<socketList.size();i++)
+                    for(int i=0; i<players.size();i++)
                     {
-                        //System.out.println(input);
-                        PrintWriter out1 = new PrintWriter(socketList.get(i).getOutputStream(), true);
-                        out1.println("player#"+clientNumber+": "+input);
+                        players.get(i).getOut().println(nick+": "+input);
                     }
                 }
             }
@@ -93,5 +104,26 @@ public class Server {
         {
             System.out.println(message);
         }
+
+        public Socket getSocket()
+        {
+            return socket;
+        }
+
+        public int getClientNumber()
+        {
+            return clientNumber;
+        }
+
+        public String getNickname()
+        {
+            return nickname;
+        }
+
+        public PrintWriter getOut()
+        {
+            return out;
+        }
     }
+
 }
