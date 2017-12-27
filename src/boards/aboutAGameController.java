6189254095f9823545d3d1e4011e0d2dae9f8c5b
AgnieszkaMarta <agnieszkamartaguzik@gmail.com>
2017-12-27 01:37:25 +0100

    aboutAGame board

---
+++ src/boards/aboutAGameController.java
@@ -0,0 +1,21 @@
+package boards;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class aboutAGameController {
+   
+	@FXML
+	public void initialize(URL location, ResourceBundle resources) {
+	}
+}
+
